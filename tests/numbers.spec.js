8edde8b119f3308593ca08d0ece159c50fdf9788
vitoraok1 <vitoraoki17@gmail.com>
2022-10-20 15:07:14 -0300

    implementing the test cases for the numbers function

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -16,10 +16,18 @@ const numbers = require('../src/numbers');
 
 describe('2 - Implemente os casos de teste para a função `numbers`', () => {
   it('Verifica se a função `numbers` retorna `true` quando o array contém apenas números e falso caso contrário', () => {
-    fail('Teste vazio!');
-    // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
-    // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
-    // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
-    // Escreva um teste em que a função recebe [' '] e retorna false
+    expect(numbers([1, 2, 3, 4, 5])).toBe(true);
+  });
+
+  it('Verifica se a função `numbers` retorna `false` ao receber o array [1, 2, `3`, 4, 5]', () => {
+    expect(numbers([1, 2, '3', 4, 5])).toBe(false);
+  });
+
+  it('Verifica se a função `numbers` retorna `false` ao receber o array [1, `a`, 3]', () => {
+    expect(numbers([1, 'a', 3])).toBe(false);
+  });
+
+  it('Verifica se a função `numbers` retorna `false` ao receber o array [` `]', () => {
+    expect(numbers([' '])).toBe(false);
   });
 });
